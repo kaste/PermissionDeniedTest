c6b8dc025ef713740cc9d64546654ebc3cfdc065
herr kaste <herr.kaste@gmail.com>
2020-01-12 12:48:05 +0100

    fixup

--- plugin.py
+++ plugin.py
@@ -17,18 +17,35 @@ def no_animations():
 
 
 def program():
-    threading.Thread(target=program).start()
+    threading.Thread(target=test).start()
 
 
 def test():
-    test_file = os.path.join(os.path.basename(__file__), 'core', 'diff.py')
+    test_file = os.path.join(os.path.dirname(__file__), 'core', 'diff.py')
+    print('test_file', test_file)
+    with open(test_file, 'r') as file:
+        text = file.read()
 
-    for t in range(10):
-        for n in range(20):
-            with open(test_file, 'r') as file:
-                text = file.read()
+    def inner():
+        for t in range(1, 10):
+            for n in range(20):
+                try:
+                    with open(test_file, 'w') as file:
+                        file.write(ADD_TEXT.format(n) + text)
+                except Exception as e:
+                    print(
+                        "Attempt {} with {} timeout failed."
+                        .format(n, 0.05 * t)
+                    )
+                    raise e from None
+                time.sleep(0.05 * t)
+                os.remove(test_file)
 
-            with open(test_file, 'w') as file:
-                file.write(ADD_TEXT.format(n) + text)
+    try:
+        inner()
+    finally:
+        time.sleep(0.5)
+        with open(test_file, 'w') as file:
+            file.write(text)
 
-            time.sleep(0.1 * t)
+        print('FIN')
